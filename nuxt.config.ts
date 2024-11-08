4e3a705909da128ffc7c7291d808492b0c7fd319
nhathdtk <nhat0x9@gmail.com>
2024-11-08 14:05:22 +0700

    install: primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primevue/themes/aura";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  ssr: true,
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  css: [
+  ],
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    // components: {
+    //   include: ["Menu"],
+    //   exclude: ['Galleria', 'Carousel']
+    // },
+    options: {
+      // ripple: false,
+      // inputVariant: "filled",
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: "p",
+          darkModeSelector: "system",
+          cssLayer: false,
+        },
+      },
+    },
+  },
+  build: {
+    transpile: ["primevue"],
+  },
+});
