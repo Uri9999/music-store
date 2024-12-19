8fcdb45ac3c48c60cebc158c3a3a6992e6dda166
nhathdtk <nhat0x9@gmail.com>
2024-12-19 10:30:44 +0700

    feat: test fcm

--- app.vue
+++ app.vue
@@ -1,9 +1,17 @@
-<script setup></script>
+<script setup>
+import { onMessage } from 'firebase/messaging';
+
+const { $messaging } = useNuxtApp();
+onMessage($messaging, (payload) => {
+    console.log('Message received. Foreground:', payload);
+    alert(`${payload.notification?.title}: ${payload.notification?.body}`);
+});
+</script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-  <Toast />
-  <ConfirmDialog></ConfirmDialog>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
+    <Toast />
+    <ConfirmDialog></ConfirmDialog>
 </template>
